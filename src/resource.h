d72d8fb7831598daae66b891e337dad5e6dc8c74
nullsrv <69965495+nullsrv@users.noreply.github.com>
2026-01-14 14:20:47 +0100

    Add program icon

--- src/resource.h
+++ src/resource.h
@@ -2,14 +2,16 @@
 // Microsoft Visual C++ generated include file.
 // Used by resource.rc
 //
-#define IDI_ICON_RED                       101
-#define IDI_ICON_GREEN                     102
+#define IDI_ICON_ACC_MAIN               100
+#define IDI_ICON_RED                    101
+#define IDI_ICON_GREEN                  102
+#define IDD_DIALOG_ABOUT                200
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
